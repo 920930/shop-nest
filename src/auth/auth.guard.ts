c161211553c45dbc2563be0382693efd46a1a3b3
iyunx <iyunx@qq.com>
2023-06-05 14:55:10 +0800

    feat: auth bug

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -1,4 +1,9 @@
-import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 // import { Observable } from 'rxjs';
 import { JwtService } from '@nestjs/jwt';
@@ -7,27 +12,30 @@ import { IS_PUBLIC } from './decorators/public.decorator';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(private jwtService: JwtService, private reflector: Reflector){}
+  constructor(private jwtService: JwtService, private reflector: Reflector) {}
   async canActivate(context: ExecutionContext): Promise<boolean> {
     // 是否需要jwt认证
-    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC, [context.getHandler(), context.getClass()])
-    if(isPublic) return true;
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    if (isPublic) return true;
 
     const request = context.switchToHttp().getRequest();
     const token = this.extractTokenFromHeader(request);
-    if(!token) throw new UnauthorizedException();
+    if (!token) throw new UnauthorizedException();
 
-    try{
-      const payload = await this.jwtService.verifyAsync(token, { secret: ''});
+    try {
+      const payload = await this.jwtService.verifyAsync(token, { secret: '' });
       request['user'] = payload;
-    }catch{
+    } catch {
       throw new UnauthorizedException();
     }
     return true;
   }
 
-  private extractTokenFromHeader(request: Request){
+  private extractTokenFromHeader(request: Request): null | string {
     const [type, token] = request.headers.authorization?.split(' ') ?? [];
-    return type === 'Bearer' ? token : null
+    return type === 'Bearer' ? token : null;
   }
 }
--- src/auth/dto/create-auth.dto.ts
+++ src/auth/dto/create-auth.dto.ts
@@ -1,17 +1,17 @@
-import { IsString, IsNotEmpty, Length } from "class-validator";
+import { IsString, IsNotEmpty, Length } from 'class-validator';
 
 export class CreateAuthDto {
   @IsNotEmpty({
-    message: '用户名不能为空'
+    message: '用户名不能为空',
   })
   @IsString({
-    message: '用户名只能是字符串'
+    message: '用户名只能是字符串',
   })
   name: string;
 
   @IsString()
   @Length(6, 20, {
-    message: `haha123: $value`
+    message: `haha123: $value`,
   })
   password: string;
 
