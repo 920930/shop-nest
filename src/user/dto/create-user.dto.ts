e6758fbac7398b052a55567d14eca4e29ea419ac
iyunx <iyunx@qq.com>
2023-06-19 20:11:12 +0800

    feat: argon2

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,13 @@
-export class CreateUserDto {}
+import { IsNotEmpty, IsPhoneNumber } from 'class-validator';
+
+export class CreateUserDto {
+  @IsNotEmpty()
+  name: string;
+
+  @IsPhoneNumber('CN')
+  @IsNotEmpty()
+  phone: string;
+
+  @IsNotEmpty()
+  password: string;
+}
--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -1,4 +1,5 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { Entity, Column, PrimaryGeneratedColumn, BeforeInsert } from 'typeorm';
+import argon2 from 'argon2';
 @Entity()
 export class User {
   @PrimaryGeneratedColumn()
@@ -8,5 +9,13 @@ export class User {
   name: string;
 
   @Column()
+  phone: string;
+
+  @Column()
   password: string;
+
+  @BeforeInsert()
+  async before() {
+    this.password = await argon2.hash(this.password);
+  }
 }
